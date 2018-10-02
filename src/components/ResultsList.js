db88389c75d01988831230c002f7961090a7ce16
Joshua R <joshua.rabiu@gmail.com>
2018-10-01 21:50:12 -0500

    decoded input query on results page

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -15,7 +15,7 @@ export const ResultsList = ({ results, outline, screenshots, query }) => {
 
   return (
     <div>
-      <div className="top-bar" ><input defaultValue={query} onKeyPress={(e) => handleKey(e, 'reset')} /><img className="glass" src={glass} /></div>
+      <div className="top-bar" ><input defaultValue={decodeURI(query)} onKeyPress={(e) => handleKey(e, 'reset')} /><img className="glass" src={glass} /></div>
       <Outline outline={outline} />
       <InfiniteScroll
         dataLength={len + 1}

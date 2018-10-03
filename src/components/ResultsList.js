9e9f8d136115eafe2455d04c22ecf048a01e2387
Joshua R <joshua.rabiu@gmail.com>
2018-10-03 13:31:53 -0500

    css fixes

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -16,8 +16,10 @@ export const ResultsList = ({ results, outline, screenshots, query }) => {
 
   return (
     <div>
-      <div className="top-bar" ><Link to="/" target="_self"><h3 className="heading-1">Spresso</h3><h3 className="heading-2">Search</h3></Link>
+      <div className="top-bar" 
+      ><Link to="/" target="_self"><h3 className="heading-1">Spresso</h3><h3 className="heading-2">Search</h3></Link>
         <input defaultValue={decodeURI(query)} onKeyPress={e => handleKey(e, 'reset')} onChange={e => setQuery(e)} /><img onClick={() => search('reset')} className="glass" src={glass} />
+        <Link to="/about" className="about-bar">About</Link>
       </div>
       <Outline outline={outline} />
       <InfiniteScroll

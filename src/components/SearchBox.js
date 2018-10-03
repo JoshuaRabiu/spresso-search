29fc53e79ec43d337d1e111879abd69c19871078
Joshua R <joshua.rabiu@gmail.com>
2018-10-02 20:53:40 -0500

    removed unnecessary props

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,7 +5,7 @@ import glass from '../images/glass.svg';
 
 
 
-export const SearchBox = ({results, query}) => (
+export const SearchBox = () => (
 		<div className="home">
 			<h1 className="home-logo">Spresso</h1> <h1 className="home-logo-2">Search</h1>
 			<input onChange={e => setQuery(e)} onKeyPress={e => handleKey(e)} autoFocus /><img onClick={search} className="glass" src={glass}/>

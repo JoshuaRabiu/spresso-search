fafc69b1aa09b187f040b7e79fee9ecbf873fcb9
Joshua R <joshua.rabiu@gmail.com>
2018-10-04 14:21:22 -0500

    css fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Home from './containers/Home';
 import { About } from './components/About';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css'
 
 export const App = () => (
   <div>
-    <Link className="about" to="/about">About</Link>
     <Switch>
       <Route exact={true} path="/" component={Home} />
       <Route path="/about" component={About} />
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/SearchBox.css';
 import { search, handleKey, setQuery } from '../actions/index';
 import glass from '../images/glass.svg';
 
 export const SearchBox = () => (
-	<div className="home">
-		<h1 className="home-logo">Spresso</h1> <h1 className="home-logo-2">Search</h1>
-		<input onChange={(e) => setQuery(e)} onKeyPress={(e) => handleKey(e)} autoFocus />
-		<img onClick={search} className="glass" alt="magnifying glass" src={glass} />
+	<div>
+		<Link className="about" to="/about">
+			About
+		</Link>
+		
+		<div className="home">
+			<h1 className="home-logo">Spresso</h1> <h1 className="home-logo-2">Search</h1>
+			<input onChange={e => setQuery(e)} onKeyPress={e => handleKey(e)} autoFocus />
+			<img onClick={search} className="glass" alt="magnifying glass" src={glass} />
+		</div>
 	</div>
 );

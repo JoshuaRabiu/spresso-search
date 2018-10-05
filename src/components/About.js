4cc9bffdf6672ec1b6842f2a6462361dff1f6e2a
Joshua R <joshua.rabiu@gmail.com>
2018-10-05 01:35:02 -0500

    added editors to gitignore, started about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,9 +8,12 @@ export const About = () => (
 			<h3 className="home-1">Spresso</h3>
 			<h3 className="home-2">Search</h3>
 		</Link>
-		<div className="home">
+		<div className="center">
 			<h4>About</h4>
-			<p>Text TextTextTextTextTextText</p>
+			<p>
+				Spresso Search is a visual metasearch engine. It scrapes results from google and obtains their meta-data
+				using the <a href="https://github.com/matthewmueller/x-ray">node x-ray</a> library. It also has a text-outline feature (powered by <a href="https://github.com/ageitgey/node-unfluff">node-unfluff</a>) that allows users to read the contents of a web page in clean, formatted text without leaving the Spresso Search site.
+			</p>
 		</div>
 	</div>
 );

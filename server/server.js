923c4c2ae52ff003447a85b958b600466887d707
Joshua R <joshua.rabiu@gmail.com>
2018-10-08 23:38:07 -0500

    code formatting

--- server/server.js
+++ server/server.js
@@ -9,14 +9,14 @@ const compression = require('compression');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const app = express();
-app.use(compression())
+app.use(compression());
 app.use(express.static(path.resolve('../', 'build')));
-
 app.use(bodyParser.text());
-const port = process.env.PORT || 8080;
 app.use(logger('dev'));
 app.use('/search', SearchController);
 app.use('/outline', OutlineController);
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);

266cbfa69a0b0c1114cd3d2600ed963bfe453a57
Zerg Mane <joshua.rabiu@gmail.com>
2019-04-26 18:54:04 -0500

    Converted tabs to spaces

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -4,46 +4,46 @@ import { SearchBox } from '../components/SearchBox';
 import { ResultsView } from '../components/ResultsView';
 
 export interface IHomeProps {
-	results?: any[];
-	loadingStatus?: boolean;
-	outline?: any;
-	query: string;
-	counter?: number;
-	screenshots?: any;
+  results?: any[];
+  loadingStatus?: boolean;
+  outline?: any;
+  query: string;
+  counter?: number;
+  screenshots?: any;
 }
 
 export const Home: React.StatelessComponent<IHomeProps> = ({
-	results,
-	loadingStatus,
-	outline,
-	query,
-	screenshots
+  results,
+  loadingStatus,
+  outline,
+  query,
+  screenshots
 }: IHomeProps) => {
-	if (loadingStatus === true) {
-		return <ResultsView query={query} loadingStatus={loadingStatus} outline={outline} />;
-	} else if (loadingStatus === false && !!results) {
-		return (
-			<ResultsView
-				results={results}
-				outline={outline}
-				screenshots={screenshots}
-				query={query}
-				loadingStatus={loadingStatus}
-			/>
-		);
-	}
-	return <SearchBox />;
+  if (loadingStatus === true) {
+    return <ResultsView query={query} loadingStatus={loadingStatus} outline={outline} />;
+  } else if (loadingStatus === false && !!results) {
+    return (
+      <ResultsView
+        results={results}
+        outline={outline}
+        screenshots={screenshots}
+        query={query}
+        loadingStatus={loadingStatus}
+      />
+    );
+  }
+  return <SearchBox />;
 };
 
 const mapStateToProps = (state: IHomeProps): IHomeProps => {
-	return {
-		results: state.results,
-		loadingStatus: state.loadingStatus,
-		outline: state.outline,
-		query: state.query,
-		counter: state.counter,
-		screenshots: state.screenshots
-	};
+  return {
+    results: state.results,
+    loadingStatus: state.loadingStatus,
+    outline: state.outline,
+    query: state.query,
+    counter: state.counter,
+    screenshots: state.screenshots
+  };
 };
 
 export default connect(mapStateToProps)(Home);
